d87fd5ea5a7d3872009c9aaaf0ebee2b551712e8
Yanqing Song <38831672+yanqing-song@users.noreply.github.com>
2024-10-31 13:50:26 +0000

    update homework 5

--- 05-deployment/predict-test.py
+++ 05-deployment/predict-test.py
@@ -1,8 +1,8 @@
 import requests
 
 url = 'http://localhost:9696/predict'
-client = {"job": "student", "duration": 280, "poutcome": "failure"}
-# client = {"job": "management", "duration": 400, "poutcome": "success"}
+# client = {"job": "student", "duration": 280, "poutcome": "failure"}
+client = {"job": "management", "duration": 400, "poutcome": "success"}
 response = requests.post(url, json=client).json()
 print(response)
 
